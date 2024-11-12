dc1ed830953295ab1a996f27326de24fd8ac183c
Sam Hall <samhall@swin.edu.au>
2024-11-12 15:40:24 +1100

    Modified model saving, added playground

--- playground.py
+++ playground.py
@@ -1,40 +1,35 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
-
 import tensorflow as tf
-from PIL import Image
+from models.model import deepJSCC
+import configparser
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
-import configparser
-
-from models.model import deepJSCC
-
 from utils.datasets import dataset_generator
 
 def main():
-    args = parse_args()
-    check_gpu()
 
-    params = read_config(args.config_file, args.config_name)
+    single_image_pass = False
+
+    params = read_config('config/config.txt', 'default')
 
     for key, value in vars(params).items():
         print(f"{key} = {value}, Type: {type(value)}")
 
-    train_ds, test_ds = prepare_dataset(params.batch_size)
-
-    EXPERIMENT_NAME = params.experiment_name
-    print(f'Running {EXPERIMENT_NAME}')
-
-    
     model = deepJSCC(
-        has_gdn=params.has_gdn,
-        num_symbols=params.data_size,
-        snrdB=params.train_snrdb,
-        channel=params.channel_type
-    )
-    #display_image(train_ds)
+            has_gdn=params.has_gdn,
+            num_symbols=params.data_size,
+            snrdB=2,
+            channel=params.channel_type
+        )
+
+    input_shape = (32,32,3)
+    dummy_input = np.zeros((1,*input_shape))
+    model(dummy_input)
+
+    model.load_weights('models/default_100.h5')
+
+
+    model.summary()
+
 
     def psnr(y_true, y_pred):
         return tf.image.psnr(y_true, y_pred, max_val=1)
@@ -49,41 +44,32 @@ def main():
         ]
     )
 
-    model.build(input_shape=(None, 32, 32, 3))
-    model.summary()
     
-    if args.ckpt is not None:
-        model.load_weights(args.ckpt)
 
-    save_ckpt = [
-      tf.keras.callbacks.ModelCheckpoint(
-          filepath=f"./ckpt/{EXPERIMENT_NAME}_" + "{epoch}",
-          save_best_only=True,
-          monitor="val_loss",
-          save_weights_only=True,
-          options=tf.train.CheckpointOptions(
-              experimental_io_device=None, experimental_enable_async_checkpoint=True
-          )
-      )
-  ]
+    if single_image_pass:
+        image_path = 'dataset/CIFAR10/test/bird/0000.jpg'
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_jpeg(image, channels=3)
+        #image = tf.image.resize(image, [28, 28])  # Resize to match your autoencoder input size
+        image = tf.cast(image, tf.float32)
+        image = image / 255.0  # Normalize the image
+    
+        image = np.expand_dims(image, axis=0)  # Add batch dimension
 
-    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=f'logs/{EXPERIMENT_NAME}')
-    history = model.fit(
-        train_ds,
-        initial_epoch=params.initial_epoch,
-        epochs=params.epochs,
-        callbacks=[tensorboard, save_ckpt],
-        validation_data=test_ds,
-    )
+        reconstructed_image = model(image)
 
-    model.save_weights('models/' + f"{EXPERIMENT_NAME}_" + f"{params.epochs}" + '.h5')
-    #model.save('models/' + f"{EXPERIMENT_NAME}_" + f"{params.epochs}" + '.h5')
 
-def check_gpu():
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    print("Num GPUs Available: ", len(physical_devices))
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        print(reconstructed_image.shape)
+        
+        save_image(image, 'input_image.jpg')
+        save_image(reconstructed_image, 'output_image.jpg')
+    else:
+        train_ds, test_ds = prepare_dataset(params.batch_size)
+        loss = model.evaluate(test_ds)
+        print(f"Loss: {loss}")   
 
+
+    
 
 def prepare_dataset(BATCH_SIZE):
     AUTO = tf.data.experimental.AUTOTUNE
@@ -140,28 +126,25 @@ def read_config(config_file, config_name):
         return params
     else:
         raise ValueError(f"Configuration '{config_name}' not found in {config_file}")
+    
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n",'--config_name', type=str, default='default',help='use config within config file'),
-    parser.add_argument("-c",'--config_file', type=str, default='config/config.txt',help='config file location'),
-    parser.add_argument("-p",'--ckpt', type=str,help='checkpoint file')
+def save_image(image, filename):
+    image = tf.squeeze(image)  # Remove batch dimension
+    image = tf.clip_by_value(image, 0.0, 1.0)  # Ensure pixel values are in [0, 1]
+    image = tf.image.convert_image_dtype(image, tf.uint8)
+    encoded_image = tf.io.encode_jpeg(image)
+    tf.io.write_file(filename, encoded_image)
 
-    return parser.parse_args()
 
-def display_image(dataset):
+def display_image(image):
     #denormalise and save example image to disk
     
-    i=0
-    for images, labels in dataset.take(1):  # Take one batch from the dataset
-        for i in range(9):
-            example_image = images[i] * 255.0
+            example_image = image * 255.0
             example_image = example_image.numpy().astype(np.uint8)  # Convert to numpy array
             img = Image.fromarray(example_image)  # Create an Image object
-            filename = 'example_images/example_image_' + str(i) + '.png' 
+            filename = 'example_images/in_image.png' 
             img.save(filename)  # Save the image to disk
             
-    
 
 
 if __name__ == "__main__":
